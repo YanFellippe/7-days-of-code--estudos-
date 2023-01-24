5ad18df51f1ccc72c55f24dce9767e816b644abf
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2023-01-23 21:33:09 -0300

    Add files via upload

---
+++ 7dayscode/2-challenge.js
@@ -0,0 +1,14 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+const msg = `"Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!"!`;
+
+alert(msg);
+
+const gosta = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 - SIM ou 2 - NÃO`);
+if (gosta == 1){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}
+if (gosta == 2){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?");
+}
